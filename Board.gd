1e8b06a63b374ffe535de68a8eede1d9b158a204
Giordano Vicente <22085029+giordanorn@users.noreply.github.com>
2020-11-25 16:04:44 -0300

    Função de printar o quadro

--- Board.gd
+++ Board.gd
@@ -10,24 +10,28 @@ var board:Array
 
 func _ready() -> void:
 	board = BASE
-	print(board)
+	print_board()
 
 
 func _process(delta) -> void:
 	if Input.is_action_just_pressed("ui_left"):
 		spawn()
+		print_board()
 	elif Input.is_action_just_pressed("ui_down"):
 		spawn()
+		print_board()
 	elif Input.is_action_just_pressed("ui_right"):
 		spawn()
+		print_board()
 	elif Input.is_action_just_pressed("ui_up"):
+		print_board()
 		spawn()
 
 
 func get_score() -> int:
 	var sum:int = 0
 	for line in board:
-		for val in board:
+		for val in line:
 			sum += val
 	return sum
 
@@ -39,8 +43,15 @@ func spawn() -> void:
 	while (is_occupied(x,y)):
 		x = rng.randi_range(0,3)
 		y = rng.randi_range(0,3)
-	var spawned_value = rng.randi_range(1,2) * 2
+	var spawned_value = 2
 	board[x][y] = spawned_value
+
 
 func is_occupied(x:int, y:int) -> bool:
 	return board[x][y] > 0
+
+
+func print_board() -> void:
+	for line in board:
+		print (line)
+	print ("Score: ", get_score())
